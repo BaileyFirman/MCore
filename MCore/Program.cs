228adfc1e333ed18ea97dbcf5fe3c8cbd012245a
BaileyFirman <bailey.firman@nintex.com>
2020-10-22 23:03:50 +1300

    Initial commit

---
+++ MCore/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MCore
+{
+    class Program
+    {
+        private static int _stackSize = 128;
+        private static int _stackStart = 1024 - 128;
+        static void Main(string[] args)
+        {
+            var memory = new Memory();
+            var stack = new Stack(memory, _stackSize, _stackStart);
+            var registers = new Registers();
+            var runtime = new Runtime(memory, stack, registers);
+
+            runtime.Start();
+        }
+    }
+}
