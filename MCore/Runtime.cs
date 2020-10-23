819e9449d8044e31297feb87238fd848e9d6f9d0
BaileyFirman <bailey.firman@nintex.com>
2020-10-23 19:51:40 +1300

    Added initial program, Ldloc, Mul, and Div

--- MCore/Runtime.cs
+++ MCore/Runtime.cs
@@ -26,7 +26,9 @@ namespace MCore
                 var offset = Operate(opcode);
                 _pc += offset;
             }
-            _memory.Dump(1024 - 128, 16);
+            _memory.Dump(1024 - 128, 8);
+            Console.WriteLine();
+            // _registers.Dump(0, 16);
             Console.WriteLine("MCORE::RUNTIME::FINISH");
         }
 
@@ -35,6 +37,10 @@ namespace MCore
             return opcode switch
             {
                 0x00 => Nop(),
+                0x06 => Ldloc_0(),
+                0x07 => Ldloc_1(),
+                0x08 => Ldloc_2(),
+                0x09 => Ldloc_3(),
                 0x0A => Stloc_0(),
                 0x0B => Stloc_1(),
                 0x0C => Stloc_2(),
@@ -47,6 +53,9 @@ namespace MCore
                 0x1D => Ldc_I4_7(),
                 0x1E => Ldc_I4_8(),
                 0x1F => Ldc_I4_S(),
+                0x20 => Ldc_I4(),
+                0x5A => Mul(),
+                0x5B => Div(),
                 _ => Nop()
             };
         }
@@ -56,8 +65,9 @@ namespace MCore
         private int Stloc(int offset)
         {
             var value = _stack.Pop();
-            var storeLocation = _stack.FramePointer + offset;
-            _memory.Write(storeLocation, value);
+            // var storeLocation = _stack.FramePointer + offset;
+            // var storeLocation = _registers +
+            _registers.Write(offset, value);
             return 1;
         }
         private int Stloc_0() => Stloc(0);
@@ -72,20 +82,40 @@ namespace MCore
             _stack.Push(value);
             return 2;
         }
-        private int Ldc_I4(int value)
+        private int Ldc_I4()
+        {
+            var next = _pc + 1;
+            var value = _memory.Read(next);
+            _stack.Push(value);
+            return 2;
+        }
+        private int Ldc_I4_Base(int value)
         {
             _stack.Push(value);
             return 1;
         }
-        private int Ldc_I4_0() => Ldc_I4(0);
-        private int Ldc_I4_1() => Ldc_I4(1);
-        private int Ldc_I4_2() => Ldc_I4(2);
-        private int Ldc_I4_3() => Ldc_I4(3);
-        private int Ldc_I4_4() => Ldc_I4(4);
-        private int Ldc_I4_5() => Ldc_I4(5);
-        private int Ldc_I4_6() => Ldc_I4(6);
-        private int Ldc_I4_7() => Ldc_I4(7);
-        private int Ldc_I4_8() => Ldc_I4(8);
+        private int Ldc_I4_0() => Ldc_I4_Base(0);
+        private int Ldc_I4_1() => Ldc_I4_Base(1);
+        private int Ldc_I4_2() => Ldc_I4_Base(2);
+        private int Ldc_I4_3() => Ldc_I4_Base(3);
+        private int Ldc_I4_4() => Ldc_I4_Base(4);
+        private int Ldc_I4_5() => Ldc_I4_Base(5);
+        private int Ldc_I4_6() => Ldc_I4_Base(6);
+        private int Ldc_I4_7() => Ldc_I4_Base(7);
+        private int Ldc_I4_8() => Ldc_I4_Base(8);
+
+        private int Ldloc_Base(int position)
+        {
+            //var offset = _stack.FramePointer + position;
+            var value = _registers.Read(position);
+            _stack.Push(value);
+            return 1;
+        }
+
+        private int Ldloc_0() => Ldloc_Base(0);
+        private int Ldloc_1() => Ldloc_Base(1);
+        private int Ldloc_2() => Ldloc_Base(2);
+        private int Ldloc_3() => Ldloc_Base(3);
         
         private int Ldarg_S()
         {
@@ -104,13 +134,38 @@ namespace MCore
             return 2;
         }
 
+        private int Div()
+        {
+            var denominator = _stack.Pop();
+            var neumerator = _stack.Pop();
+            var result = neumerator / denominator;
+            _stack.Push(result);
+            return 1;
+        }
+
+        private int Mul()
+        {
+            var a = _stack.Pop();
+            var b = _stack.Pop();
+            var result = a * b;
+            _stack.Push(result);
+            return 1;
+        }
+
         private int[] program = new int[]
         {
+            0x00,
+            0x20, 400,
             0x0A,
+            0x20, 20,
             0x0B,
+            0x06,
+            0x07,
+            0x5B,
             0x0C,
-            0x0D,
-            0x0B,
+            0x08,
+            0x1B,
+            0x5A,
         };
     }
 }
